b931f418fbe12819d415e0d9cb9c832a8b7d934c
Joshua Zahner <joshuazahner@gmail.com>
2016-12-09 07:48:55 -0600

    Initial Commit

---
+++ Pattonville-School-District-AppTests/Pattonville_School_District_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pattonville_School_District_AppTests.swift
+//  Pattonville-School-District-AppTests
+//
+//  Created by Joshua Zahner on 12/9/16.
+//  Copyright © 2016 Joshua Zahner. All rights reserved.
+//
+
+import XCTest
+@testable import Pattonville_School_District_App
+
+class Pattonville_School_District_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
