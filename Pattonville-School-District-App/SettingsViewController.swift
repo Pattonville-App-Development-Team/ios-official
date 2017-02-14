2d4c5c3b3254f42a891a3e9a7da1d951bf1b26b0
MustaphaB <barreim213@gmail.com>
2017-02-14 17:33:06 -0600

    Steppers change the value of label

--- Pattonville-School-District-App/SettingsViewController.swift
+++ Pattonville-School-District-App/SettingsViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 /// The UITableViewController that is used to display the list of application settings
 class SettingsViewController: UITableViewController{
     
+    @IBOutlet weak var newsStepper: UIStepper!
+    
+    
     @IBOutlet weak var carouselStepper: UIStepper!
     
     
-    @IBOutlet weak var newsStepper: UIStepper!
+    @IBOutlet weak var newsCounter: UILabel!
     
+    @IBOutlet weak var carouselCounter: UILabel!
+    
+    @IBAction func newsValueChanged(_ sender: UIStepper) {
+        
+        newsCounter.text = Int(sender.value).description
+    }
+    
+    @IBAction func carouselValueChanged(_ sender: UIStepper) {
+        carouselCounter.text = Int(sender.value).description
+    }
     
     
     override func viewDidLoad() {
