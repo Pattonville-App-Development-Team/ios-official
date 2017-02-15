698940e43c6176a88b65113a37d34f212fdb6263
MicahThompkins <iosdeveloper000@gmail.com>
2017-02-14 18:26:32 -0600

    Deleted the add to calendar functionality, committing to switch to master for board meeting

--- Pattonville-School-District-App/CalendarEventDetailController.swift
+++ Pattonville-School-District-App/CalendarEventDetailController.swift
@@ -52,27 +52,10 @@ class CalendarEventDetailController: UIViewController, EKEventEditViewDelegate{
     }
     
     @IBAction func add(sender: UIButton){
-        let store = EKEventStore()
-        store.requestAccess(to: .event) {(granted, error) in
-            if !granted { return }
-            
-            let ekEvent = EKEvent(eventStore: store)
-            ekEvent.title = self.event.name!
-            ekEvent.startDate = self.event.date!
-            ekEvent.endDate = self.event.date!
-            ekEvent.calendar = store.defaultCalendarForNewEvents
-            
-            do {
-                try store.save(ekEvent, span: .thisEvent, commit: true)
-                
-                let alert = UIAlertController(title: "Event Added to Calendar", message: "The event \(ekEvent.title) was added to your calendar!", preferredStyle: UIAlertControllerStyle.alert)
-                alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                
-            } catch {
-                print(error)
-            }
-        }
+        print("old useless method accessed")
+        let alert = UIAlertController(title: "Useless", message: "old useless method accessed", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
     ///Sets up the look of the ViewController upon loading. Sets the eventName, eventLocation, eventDate, and eventTime UILabels to the corresponding values of the event variable.
