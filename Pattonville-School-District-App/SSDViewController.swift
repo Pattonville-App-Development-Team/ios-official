aecf8fb73555f9f14859cd822bde568a6c4fb7c2
Kevin Bowers <kevin.p.bowers@gmail.com>
2017-02-27 11:22:36 -0600

    Revert "Started using Navigation bar title in SSD.storyboard and switched everything else to auto constraints. Need to find how to scale text based on device/screen size."
    This reverts commit f0f01aa1c791fa445fa036c8542e825b60ee7152.

--- Pattonville-School-District-App/SSDViewController.swift
+++ Pattonville-School-District-App/SSDViewController.swift
@@ -12,6 +12,10 @@ import UIKit
 
 class SSDViewController: UIViewController{
     
+    //var schools = SchoolsArray.init().allSchools
+    
+    //var staffList = StaffArray.init().staffList
+    @IBOutlet weak var schoolName: UILabel!
     @IBOutlet weak var address: UILabel!
     @IBOutlet weak var location: UILabel!
     @IBOutlet weak var mainNumber: UILabel!
@@ -30,17 +34,16 @@ class SSDViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        print(indexOfSchool)
         SSDViewController.staticSchoolIndex = indexOfSchool
         let school = SchoolsArray.allSchools[indexOfSchool]
-        self.navigationItem.title = school.name
+        schoolName.text = school.name
         address.text = school.address
         location.text = school.city + ", " + school.state + " " + school.zip
         mainNumber.text = "Main - " + school.mainNumber
         attendanceNumber.text = "Attendance - " + school.attendanceNumber
         faxNumber.text = "Fax - " + school.faxNumber
         schoolPicture.image = UIImage(named: school.schoolPicture)
-        
-        
         
     }
     
