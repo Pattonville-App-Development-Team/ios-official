45734999bdba69b5ca7614c94712fc7b92af67a1
Extra Mac <extramac@Extras-MacBook-Pro.local>
2017-03-08 12:35:18 -0600

    Committing to merge

--- Pattonville-School-District-App/AppDelegate.swift
+++ Pattonville-School-District-App/AppDelegate.swift
@@ -36,12 +36,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         } else {
             print("First launch, setting UserDefault.")
-            
-            
-            
+            let value = 3
+            UserDefaults.standard.set(value, forKey:"recentNews")
+            UserDefaults.standard.set(value, forKey: "upcomingNews")
+            UserDefaults.standard.set(value, forKey: "pinnedEvents")
+
         }
         
         return true
+
         
     }
     
