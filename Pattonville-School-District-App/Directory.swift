cf8c2858de68b3c841363f19e147aaab93f16980
Kevin Bowers <kevin.p.bowers@gmail.com>
2017-04-10 12:10:32 -0500

    Added functionality to save the directory Dictionary object to the device and re downloads only once a year

--- Pattonville-School-District-App/Directory.swift
+++ Pattonville-School-District-App/Directory.swift
@@ -19,8 +19,8 @@ class Directory {
     
     /// Reads data from file and populates the directory Dictionary
     init() {
-        //        studentDirectoryCSVContents = readDataFromURL(url: "https://forms.psdr3.org/psdapp/directory.csv")
-        studentDirectoryCSVContents = readDataFromFile(file: "directory")
+        
+//        studentDirectoryCSVContents = readDataFromFile(file: "directory")
         createStaffMembers()
     }
     
@@ -28,64 +28,98 @@ class Directory {
     ///
     /// - Parameter file: Name of the directory CSV file
     /// - Returns: String value of the directory CSV file
-    //    func readDataFromURL(url: String)-> String!{
-    //        guard let filepath = NSURL(string: url)
-    //            else {
-    //                return nil
-    //        }
-    //        do {
-    //            return try String(contentsOf: filepath as URL, encoding: String.Encoding.utf8)
-    //        } catch {
-    //            print("File Read Error for file \(filepath)")
-    //            return nil
-    //        }
-    //    }
-    
-    func readDataFromFile(file: String)-> String!{
-        guard let filepath = Bundle.main.path(forResource: file, ofType: "csv")
-            else {
+        func readDataFromURL(url: String)-> String!{
+            guard let filepath = NSURL(string: url)
+                else {
+                    return nil
+            }
+            do {
+                return try String(contentsOf: filepath as URL, encoding: String.Encoding.utf8)
+            } catch {
+                print("File Read Error for file \(filepath)")
                 return nil
+            }
         }
-        do {
-            return try String(contentsOfFile: filepath, encoding: String.Encoding.utf8)
-        } catch {
-            print("File Read Error for file \(filepath)")
-            return nil
-        }
-    }
+    
+//    func readDataFromFile(file: String)-> String!{
+//        guard let filepath = Bundle.main.path(forResource: file, ofType: "csv")
+//            else {
+//                return nil
+//        }
+//        do {
+//            return try String(contentsOfFile: filepath, encoding: String.Encoding.utf8)
+//        } catch {
+//            print("File Read Error for file \(filepath)")
+//            return nil
+//        }
+//    }
 
 
     /// Creates objects of type StaffMember from each line of the directory CSV file
     func createStaffMembers() {
-
-        let input = studentDirectoryCSVContents
         
-        // Separates each line of the directory CSV file
-        var lines: [String] = input.components(separatedBy: "\n")
         
-        // Removes the first line which is the name of each category for a StaffMember
-        lines.removeFirst()
         
-        // Creates a new StaffMember from each non-empty line of the data
-        for line in lines{
-            if line.characters.count > 0{
-                let staffParams: [String] = line.components(separatedBy: ",")
-                let staffMember = StaffMember(values: staffParams)
-                
-                // Changes first and last names to have first letter of the name capitalized and all other letters lowercased
-                staffMember.fName = staffMember.fName.capitalized(with: NSLocale.current)
-                staffMember.lName = staffMember.lName.capitalized(with: NSLocale.current)
-                
-                // Adds a new key, specifying location, to the directoryDictionary if one is not present
-                if(Directory.directoryDictionary.index(forKey: staffMember.directoryKey) == nil){
-                    Directory.directoryDictionary[staffMember.directoryKey] = []
-                }
-                
-                // Adds the newly made StaffMember to its respective StaffMember array based on location
-                Directory.directoryDictionary[staffMember.directoryKey]?.append(staffMember)
-    
-            }
+        let mostRecentSave: Date
+        
+        // Get most recent save Date()
+        if let recent = UserDefaults.standard.object(forKey: "lastDirectoryUpdate") as! Date?{
+            mostRecentSave = recent
+        }else{
+            mostRecentSave = Date()
         }
+        
+        var dateComponent = DateComponents()
+        dateComponent.year = -1
+        
+        // Find the date for one year ago
+        let lastYear = NSCalendar(calendarIdentifier: .gregorian)?.date(byAdding: dateComponent, to: Date(), options: [])
+        
+        if NSKeyedUnarchiver.unarchiveObject(withFile: fileURL.path!) as? [String:[StaffMember]] != nil {
+            
+        }
+        
+        //If the most recent save time is longer than one year ago OR the object in the archived space is nil
+        if mostRecentSave < lastYear! || NSKeyedUnarchiver.unarchiveObject(withFile: fileURL.path!) as? [String:[StaffMember]] != nil{
+            
+            Directory.directoryDictionary = NSKeyedUnarchiver.unarchiveObject(withFile: fileURL.path!) as! [String : [StaffMember]]
+            print("getting from saved")
+            
+        } else {
+        
+            let input = readDataFromURL(url: "https://forms.psdr3.org/psdapp/directory.csv")
+            
+            // Separates each line of the directory CSV file
+            var lines: [String] = input!.components(separatedBy: "\n")
+            
+            // Removes the first line which is the name of each category for a StaffMember
+            lines.removeFirst()
+            
+            // Creates a new StaffMember from each non-empty line of the data
+            for line in lines{
+                if line.characters.count > 0{
+                    let staffParams: [String] = line.components(separatedBy: ",")
+                    let staffMember = StaffMember(values: staffParams)
+                    
+                    // Changes first and last names to have first letter of the name capitalized and all other letters lowercased
+                    staffMember.fName = staffMember.fName.capitalized(with: NSLocale.current)
+                    staffMember.lName = staffMember.lName.capitalized(with: NSLocale.current)
+                    
+                    // Adds a new key, specifying location, to the directoryDictionary if one is not present
+                    if(Directory.directoryDictionary.index(forKey: staffMember.directoryKey) == nil){
+                        Directory.directoryDictionary[staffMember.directoryKey] = []
+                    }
+                    
+                    // Adds the newly made StaffMember to its respective StaffMember array based on location
+                    Directory.directoryDictionary[staffMember.directoryKey]?.append(staffMember)
+                    
+                }
+            }
+        
+            saveToFile()
+        
+        }
+        
     }
     
     /// Sorts all staff members by their rank
@@ -112,21 +146,23 @@ class Directory {
         }
     }
     
+    // The URL of the cache file
+    let fileURL: NSURL = {
+        let directories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let document = directories.first!
+        
+        return document.appendingPathComponent("staffMember.archive") as NSURL
+        
+    }()
+    
+    /// Save directoryDictionary to the Cache File
+    /// - returns: if saving succeeded
+    func saveToFile(){
+        NSKeyedArchiver.archiveRootObject(Directory.directoryDictionary, toFile: fileURL.path!)
+        
+    }
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
